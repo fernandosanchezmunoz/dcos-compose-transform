15568be67548c820cc68087a9e4d02b2355a7c97
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-15 01:04:32 -0400

    WIP

--- src/dcos-compose.py
+++ src/dcos-compose.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
 	proc = subprocess.Popen( [command], stdout=subprocess.PIPE, shell=True)
 	(out, err) = proc.communicate()	
 	print("**DEBUG: Output of CONTAINER-TRANSFORM is {0}".format( out ))
-	containers = json.loads( out )
+	containers = json.loads( str(out) )
 	print("**DEBUG: containers is {0}".format( containers ))
 	#remove the trailing \n from file
 	for line in containers:
