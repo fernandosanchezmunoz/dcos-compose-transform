6b451e94e997d86dfe86008dbd5726408cd5898b
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-16 19:52:29 -0400

    WIP

--- src/marathon_pod.py
+++ src/marathon_pod.py
@@ -92,7 +92,9 @@ def adapt_apps_to_pod( apps, name, app_server_address ):
 		for portMapping in container.get('docker', {}).get('portMappings', {}):
 			endpoint = {}
 			endpoint['name'] = name+str(portMapping['containerPort'])
-			endpoint['hostPort'] = portMapping['hostPort']
+			#endpoint['hostPort'] = portMapping['hostPort']
+			#hostPort is now containerPort
+			endpoint['hostPort'] = portMapping['containerPort']
 			endpoint['protocol'] = [ portMapping['protocol'] ]
 			temp_app['endpoints'].append(endpoint)
 		temp_app['image'] = { } 
@@ -213,7 +215,8 @@ def create_artifact_from_volume( volume, app_name, app_server_address ):
 	#(out, err) = proc.communicate()
 
 	#this copies ./app
-	#test for Node.JS -- suspect it doesn't go into staging_container_path and instead it executes from staging_app_dir. Copy there	
+	#test for Node.JS -- suspect it doesn't go into staging_container_path and instead it executes from staging_app_dir. Copy there
+	host_path_to	
 	print("**DEBUG: Copy {0} into {1}".format(host_path_to_copy, staging_app_dir))
 	command = "cp -r "+host_path_to_copy+" "+staging_app_dir+"/"+last_part_of_container_path
 	proc = subprocess.Popen( command, stdout=subprocess.PIPE, shell=True)
